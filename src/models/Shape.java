4e6660bab3e26e9009ac22ce9a07f09a452f7d1b
alisher766 <154649658+alisher766@users.noreply.github.com>
2024-01-27 04:27:21 +0600

    Update Shape.java
    changed the array

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -1,12 +1,18 @@
+import java.util.ArrayList;
 public class Shape {
-    private Point[] points;
-    ArrayList<Point> points = new ArrayList<>();
+    private ArrayList<Point> points = new ArrayList<>();
+
     public Shape(Point[] points) {
-        this.points = points;
-        public void addPoint(Point point) {
-            points.add(point);
+        for (Point point : points) {
+            this.points.add(point);
         }
-        public double perimeter() {
+    }
+
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+}
+public double perimeter() {
             public double calculatePerimeter() {
                 double perimeter = 0;
                 for (int i = 0; i < points.length - 1; i++) {
@@ -19,25 +25,25 @@ public class Shape {
                     }
                     perimeter += points[points.length - 1].distanceTo(points[0]);
                     return perimeter;
-                }       
-                        }
-                        public double averageSide() {
-                            double totalSideLength = 0;
-                            for (int i = 0; i < points.length - 1; i++) {
-                                totalSideLength += points[i].distanceTo(points[i + 1]);
-                        public double getLongestSide() {
-                            double longestSide = 0;
-                            for (int i = 0; i < points.size(); i++) {
-                                Point currentPoint = points.get(i);
-                                 Point nextPoint = points.get((i + 1) % points.size());
-                                 double currentSide = currentPoint.distance(nextPoint);
-                                if (currentSide > longestSide) {
-                                        longestSide = currentSide;
-                                        }
-                                    }
-                                    totalSideLength += points[points.length - 1].distanceTo(points[0]);
-                                    return totalSideLength / points.length;
+                }
+            }
+                public double averageSide() {
+                double totalSideLength = 0;
+                for (int i = 0; i < points.length - 1; i++) {
+                    totalSideLength += points[i].distanceTo(points[i + 1]);
+                    totalSideLength += points[points.length - 1].distanceTo(points[0]);
+                    return totalSideLength / points.length; } }
 
-                                    return longestSide;
-                                }
-                            }
+                public double getLongestSide() {
+                    double longestSide = 0;
+                    for (int i = 0; i < points.size(); i++) {
+                        Point currentPoint = points.get(i);
+                        Point nextPoint = points.get((i + 1) % points.size());
+                        double currentSide = currentPoint.distance(nextPoint);
+                        if (currentSide > longestSide) {
+                            longestSide = currentSide;
+                        }
+
+                    }
+                    return longestSide;
+                }
