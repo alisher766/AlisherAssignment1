a237cca86e7c92b85cda006e396ab6171d913ff8
alisher766 <154649658+alisher766@users.noreply.github.com>
2024-01-27 02:38:52 +0600

    Update Shape.java
    readability correction

--- src/models/Shape.java
+++ src/models/Shape.java
@@ -19,36 +19,20 @@ public class Shape {
                     }
                     perimeter += points[points.length - 1].distanceTo(points[0]);
                     return perimeter;
-                }
-                public double longestSide() {
-                    double longestSide = 0;
-                    for (int i = 0; i < points.length - 1; i++) {
-                        double sideLength = points[i].distanceTo(points[i + 1]);
-                        if (sideLength > longestSide) {
-                            longestSide = sideLength;
-                        }
-                        public double getAverageSide() {
-                            int size = points.size();
-                            if (size == 0) {
-                                return 0;
-                            }
-                            longestSide = Math.max(longestSide, points[points.length - 1].distanceTo(points[0]));
-                            return longestSide;
-
-                            return calculatePerimeter() / size;
+                }       
                         }
                         public double averageSide() {
                             double totalSideLength = 0;
                             for (int i = 0; i < points.length - 1; i++) {
                                 totalSideLength += points[i].distanceTo(points[i + 1]);
-                                public double getLongestSide() {
-                                    double longestSide = 0;
-                                    for (int i = 0; i < points.size(); i++) {
-                                        Point currentPoint = points.get(i);
-                                        Point nextPoint = points.get((i + 1) % points.size());
-                                        double currentSide = currentPoint.distance(nextPoint);
-                                        if (currentSide > longestSide) {
-                                            longestSide = currentSide;
+                        public double getLongestSide() {
+                            double longestSide = 0;
+                            for (int i = 0; i < points.size(); i++) {
+                                Point currentPoint = points.get(i);
+                                 Point nextPoint = points.get((i + 1) % points.size());
+                                 double currentSide = currentPoint.distance(nextPoint);
+                                if (currentSide > longestSide) {
+                                        longestSide = currentSide;
                                         }
                                     }
                                     totalSideLength += points[points.length - 1].distanceTo(points[0]);
