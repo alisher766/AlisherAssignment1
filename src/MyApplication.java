84ef9d58cbbdce11f14af6793f49481ed5f00f47
alisher766 <154649658+alisher766@users.noreply.github.com>
2024-01-27 04:33:22 +0600

    Update MyApplication.java
    correcting my mistakes

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,40 +1,32 @@
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+
 public class MyApplication {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+
         Point[] points = new Point[10];
         for (int i = 0; i < 10; i++) {
             points[i] = new Point(Math.random() * 10, Math.random() * 10);
-            public static void main(String[] args) throws FileNotFoundException {
-                File file = new File("C:\\Users\\alish\\IdeaProjects\\alisher2305d\\src\\source.text");
-                Scanner sc = new Scanner(file);
-                Shape shape = new Shape();
-                Point firstPoint = null;
-                Point previousPoint = null;
-                while (sc.hasNext()) {
-                    double x = sc.nextDouble();
-                    double y = sc.nextDouble();
-                    Point point = new Point(x, y);
-                    shape.addPoint(point);
-                    if (previousPoint != null) {
-                        double distance = previousPoint.distance(point);
-                        System.out.println("Distance between " + previousPoint + " and " + point + " = " + distance);
-                    } else {
-                        firstPoint = point;
-                    }
-                    previousPoint = point;
-                }
-                Shape myShape = new Shape(points);
-                if (previousPoint != null && firstPoint != null) {
-                    double distance = previousPoint.distance(firstPoint);
-                    System.out.println("Distance between " + previousPoint + " and " + firstPoint + " = " + distance);
-                }
-                System.out.println("Perimeter: " + myShape.perimeter());
-                System.out.println("Longest Side: " + myShape.longestSide());
-                System.out.println("Average Side: " + myShape.averageSide());
-                //OUTPUT
-                System.out.println("Perimeter: " + shape.calculatePerimeter());
-                System.out.println("Average side: " + shape.getAverageSide());
-                System.out.println("Longest side: " + shape.getLongestSide());
-            }
         }
+        Shape shape = new Shape();
+        File file = new File("C:\\Users\\alish\\IdeaProjects\\alisher2305d\\src\\source.txt");
+        Scanner sc = new Scanner(file);
+        while (sc.hasNext()) {
+            double x = sc.nextDouble();
+            double y = sc.nextDouble();
+            shape.addPoint(new Point(x, y));
+        }
+        sc.close();
+        System.out.println("Distances between consecutive points:");
+        for (int i = 0; i < shape.getNumberOfPoints() - 1; i++) {
+            Point currentPoint = shape.getPoint(i);
+            Point nextPoint = shape.getPoint(i + 1);
+            double distance = currentPoint.distanceTo(nextPoint);
+            System.out.println("Distance between " + currentPoint + " and " + nextPoint + " = " + distance);
+        }
+        System.out.println("Perimeter: " + shape.perimeter());
+        System.out.println("Longest Side: " + shape.longestSide());
+        System.out.println("Average Side: " + shape.averageSide());
+    }
+}
